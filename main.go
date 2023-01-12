955d0366afd37124c330b398e0b3671f11283286
David Malicote <david.malicote@gmail.com>
2023-01-11 21:18:03 -0500

    trying to parse the results

--- main.go
+++ main.go
@@ -30,24 +30,45 @@ func main() {
 		fmt.Println("Error calling iam: ", err)
 		return
 	}
+	total := len(result.Users)
+	fmt.Println("Users: ", total)
 
-	count := len(result.Users)
-	fmt.Println("Users: ", count)
+	people := make([][]string, total)
+	count := 0
 
 	for _, user := range result.Users {
-		fmt.Printf("\t%v\n", *user.UserName)
+		// fmt.Printf("\t%v\n", *user.UserName)
 
-		params := &iam.ListAccessKeysInput{
+		people[count] = make([]string, 3)
+
+		AccessKeyParams := &iam.ListAccessKeysInput{
 			UserName: aws.String(*user.UserName),
 		}
-		info, err := client.ListAccessKeys(context.TODO(), params)
+		info, err := client.ListAccessKeys(context.TODO(), AccessKeyParams)
 		if err != nil {
 			fmt.Println("Error calling GetUserName: ", err)
 			return
+
 		}
 		for _, k := range info.AccessKeyMetadata {
-			fmt.Printf("\t%v\t%v\n", *k.AccessKeyId, *k.CreateDate)
+			// fmt.Printf("\t%v\t%v\n", *k.AccessKeyId, *k.CreateDate)
+			people[count] = append(people[count], *user.UserName)
+			people[count] = append(people[count], *k.AccessKeyId)
+			t := *k.CreateDate
+			//fmt.Println(t.String())
+			people[count] = append(people[count], t.String())
+			// fmt.Printf("%v\t%v\t%v\n", *user.UserName, *k.AccessKeyId, *k.CreateDate)
 		}
+		count++
+	}
+	for _, user := range people {
+		// layout := "2021-10-01 14:32:27 +0000 UTC"
+		// t := user
+		//k, err := time.Parse(layout, t)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(user)
 	}
 
 }
